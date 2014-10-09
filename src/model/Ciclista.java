f79418352a9457b0f3a051d447b9cb5226d21506
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 10:54:08 -0300

    Concertando Classes Herdeiras de Atleta que são Ciclista e Tenista

--- src/model/Ciclista.java
+++ src/model/Ciclista.java
@@ -1,5 +1,5 @@
 package model;
 
-public class Ciclista {
+public class Ciclista extends Atleta {
 
 }
--- src/model/Tenista.java
+++ src/model/Tenista.java
@@ -1,5 +1,5 @@
 package model;
 
-public class Tenista {
+public class Tenista extends Atleta{
 
 }
