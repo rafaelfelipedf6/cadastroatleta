ac59c0abe16d0ac8976e3b11204f4700b67cd249
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 10:45:11 -0300

    Adicionando Atributos da Classe Endereco

--- src/model/Endereco.java
+++ src/model/Endereco.java
@@ -1,5 +1,11 @@
 package model;
 
 public class Endereco {
+	private String logradouro;
+	private String cep;
+	private String numero;
+	private String bairro;
+	private String cidade;
+	private String estado;
 
 }
