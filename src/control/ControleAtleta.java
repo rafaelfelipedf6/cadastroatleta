f8ca8ccdc9e88f6160584a938d3286337315f725
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 11:27:14 -0300

    Adicionando Métodos Adicionar, Remover e Buscar atletas dentro do ArrayList

--- src/control/ControleAtleta.java
+++ src/control/ControleAtleta.java
@@ -8,4 +8,23 @@ public class ControleAtleta {
 	public ControleAtleta(){
 		listaAtletas = new ArrayList<Atleta>();
 	}
-}
+	public void adicionar(Atleta umAtleta){
+		
+		listaAtletas.add(umAtleta);
+		
+	}
+	public void remover(Atleta umAtleta){
+		
+		listaAtletas.remove(umAtleta);
+	
+	}
+	public Atleta buscar(String umNome){
+		
+		for (Atleta umAtleta: listaAtletas){
+			if (umAtleta.getNome().equalsIgnoreCase(umNome)){
+				return umAtleta;
+			}
+		}
+		return null;
+	}
+}	
