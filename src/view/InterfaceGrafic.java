18f10131c8eabb22941377f39e260c84161d3d98
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 11:37:44 -0300

    Adicionando Interface Grafica com WindowsBuilder

---
+++ src/view/InterfaceGrafic.java
@@ -0,0 +1,45 @@
+package view;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JMenuBar;
+import javax.swing.JTextField;
+import javax.swing.BoxLayout;
+
+public class InterfaceGrafic extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					InterfaceGrafic frame = new InterfaceGrafic();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public InterfaceGrafic() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+	}
+
+}
