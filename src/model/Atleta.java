40f30a44914dd4e5967ea0362647b2a5deb3417c
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 10:39:57 -0300

    Adicionando Getters e Setters na class Atleta

--- src/model/Atleta.java
+++ src/model/Atleta.java
@@ -6,5 +6,35 @@ public class Atleta {
 	String altura;
 	int idade;	
 	String cep;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public int getPeso() {
+		return peso;
+	}
+	public void setPeso(int peso) {
+		this.peso = peso;
+	}
+	public String getAltura() {
+		return altura;
+	}
+	public void setAltura(String altura) {
+		this.altura = altura;
+	}
+	public int getIdade() {
+		return idade;
+	}
+	public void setIdade(int idade) {
+		this.idade = idade;
+	}
+	public String getCep() {
+		return cep;
+	}
+	public void setCep(String cep) {
+		this.cep = cep;
+	}
 
 }
