babb1ae1f54fe52372b10a170d55c9e5393a879b
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 10:59:15 -0300

    Adicionando métodos getters e setters do Ciclista

--- src/model/Ciclista.java
+++ src/model/Ciclista.java
@@ -6,4 +6,30 @@ public class Ciclista extends Atleta {
 	private String recordCorrida;
 	private int aroBike;
 	
+	
+	public String getMarcaBike() {
+		return marcaBike;
+	}
+	public void setMarcaBike(String marcaBike) {
+		this.marcaBike = marcaBike;
+	}
+	public String getTempoCarreira() {
+		return tempoCarreira;
+	}
+	public void setTempoCarreira(String tempoCarreira) {
+		this.tempoCarreira = tempoCarreira;
+	}
+	public String getRecordCorrida() {
+		return recordCorrida;
+	}
+	public void setRecordCorrida(String recordCorrida) {
+		this.recordCorrida = recordCorrida;
+	}
+	public int getAroBike() {
+		return aroBike;
+	}
+	public void setAroBike(int aroBike) {
+		this.aroBike = aroBike;
+	}
+	
 }
