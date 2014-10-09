4a254c8bb9e99d170f9ca572560119dd1f69607d
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 11:54:56 -0300

    Construindo Butoes na interface Grafica

--- src/view/InterfaceGrafic.java
+++ src/view/InterfaceGrafic.java
@@ -1,5 +1,5 @@
 package view;
-
+import control.*;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 
@@ -9,6 +9,11 @@ import javax.swing.border.EmptyBorder;
 import javax.swing.JMenuBar;
 import javax.swing.JTextField;
 import javax.swing.BoxLayout;
+import java.awt.Color;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JMenu;
 
 public class InterfaceGrafic extends JFrame {
 
@@ -37,9 +42,34 @@ public class InterfaceGrafic extends JFrame {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
+		contentPane.setBackground(Color.BLACK);
+		contentPane.setForeground(Color.WHITE);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		contentPane.setLayout(new BoxLayout(contentPane, BoxLayout.X_AXIS));
+		contentPane.setLayout(null);
+		
+		JButton btnCadastrarAtleta = new JButton("Cadastrar Atleta");
+		btnCadastrarAtleta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				
+				
+			}
+		});
+		btnCadastrarAtleta.setBounds(12, 0, 211, 25);
+		contentPane.add(btnCadastrarAtleta);
+		
+		JButton btnConsultarTorneios = new JButton("Consultar Atletas");
+		btnConsultarTorneios.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		btnConsultarTorneios.setBounds(12, 26, 211, 25);
+		contentPane.add(btnConsultarTorneios);
+		
+		JButton btnCadastrarTorneios = new JButton("Cadastrar Torneios");
+		btnCadastrarTorneios.setBounds(12, 53, 211, 25);
+		contentPane.add(btnCadastrarTorneios);
+		
+		
 	}
-
 }
