6d16ff0a62c5caedfc6b2167fc3162a6f0bc65c0
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 11:01:15 -0300

    Adicionando métodos Getters e Setters da Class Tenista

--- src/model/Tenista.java
+++ src/model/Tenista.java
@@ -4,4 +4,22 @@ public class Tenista extends Atleta{
 	private String modelTaco;
 	private String marcaBolas;
 	private String campoPreferido;
+	public String getModelTaco() {
+		return modelTaco;
+	}
+	public void setModelTaco(String modelTaco) {
+		this.modelTaco = modelTaco;
+	}
+	public String getMarcaBolas() {
+		return marcaBolas;
+	}
+	public void setMarcaBolas(String marcaBolas) {
+		this.marcaBolas = marcaBolas;
+	}
+	public String getCampoPreferido() {
+		return campoPreferido;
+	}
+	public void setCampoPreferido(String campoPreferido) {
+		this.campoPreferido = campoPreferido;
+	}
 }
