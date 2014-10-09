8d39ccc831bc423ced387284bbb4f2766eae27ab
Rafael Felipe Mendonça Araújo <rafaelfelipedf6@gmail.com>
2014-10-09 12:48:40 -0300

    alterando funcoes da interface grafica

--- src/view/InterfaceGrafic.java
+++ src/view/InterfaceGrafic.java
@@ -1,75 +1,50 @@
 package view;
+
 import control.*;
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
+import javax.swing.*;
+import java.awt.*;
+public class InterfaceGrafic {
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
-import javax.swing.JMenuBar;
-import javax.swing.JTextField;
-import javax.swing.BoxLayout;
-import java.awt.Color;
-import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JMenu;
-
-public class InterfaceGrafic extends JFrame {
-
-	private JPanel contentPane;
-
-	/**
-	 * Launch the application.
-	 */
 	public static void main(String[] args) {
-		EventQueue.invokeLater(new Runnable() {
-			public void run() {
-				try {
-					InterfaceGrafic frame = new InterfaceGrafic();
-					frame.setVisible(true);
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
+		// TODO Auto-generated method stub
+		JOptionPane.showMessageDialog(null, "Bem Vindo ao Cadastro Profissional de Atletas");
+		JOptionPane.showMessageDialog(null, "Você poderá cadastrar Tenistas e Ciclistas nesta plataforma além de\nIncluir novos Torneios");
+		
+		/*Botoes do Menu Principal*/
+		JButton botaoCadastrarTenista = new JButton("Cadastrar Tenista");
+		JButton botaoCadastrarCiclista = new JButton("Cadastrar Ciclista");
+		JButton botaoSair = new JButton("Sair");
+
+	
+		
+
+		JButton botaoNome = new JButton("Nome");
+		JButton botaoAltura = new JButton("Altura");
+		JButton botaoPeso = new JButton("Peso");
+		
+		JPanel painelCadastro = new JPanel();
+		painelCadastro.add(botaoNome);
+		painelCadastro.add(botaoAltura);
+		painelCadastro.add(botaoPeso);
+		
+		
+		JPanel painel = new JPanel();
+		painel.add(botaoCadastrarTenista);
+		painel.add(botaoCadastrarCiclista);
+		painel.add(botaoSair);
+		
+		JFrame janela = new JFrame("Cadastro Atleta - 2.0");
+		janela.add(painel);
+		janela.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janela.pack();
+		janela.setVisible(true);
+		
+		JFrame janelaCadastro = new JFrame("Menu - Cadastro");
+		janelaCadastro.add(painelCadastro);
+		janelaCadastro.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		janelaCadastro.pack();
+		janelaCadastro.setVisible(false);
+		
 	}
 
-	/**
-	 * Create the frame.
-	 */
-	public InterfaceGrafic() {
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
-		contentPane = new JPanel();
-		contentPane.setBackground(Color.BLACK);
-		contentPane.setForeground(Color.WHITE);
-		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		setContentPane(contentPane);
-		contentPane.setLayout(null);
-		
-		JButton btnCadastrarAtleta = new JButton("Cadastrar Atleta");
-		btnCadastrarAtleta.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-				
-				
-			}
-		});
-		btnCadastrarAtleta.setBounds(12, 0, 211, 25);
-		contentPane.add(btnCadastrarAtleta);
-		
-		JButton btnConsultarTorneios = new JButton("Consultar Atletas");
-		btnConsultarTorneios.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent e) {
-			}
-		});
-		btnConsultarTorneios.setBounds(12, 26, 211, 25);
-		contentPane.add(btnConsultarTorneios);
-		
-		JButton btnCadastrarTorneios = new JButton("Cadastrar Torneios");
-		btnCadastrarTorneios.setBounds(12, 53, 211, 25);
-		contentPane.add(btnCadastrarTorneios);
-		
-		
-	}
 }
